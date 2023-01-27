a8d207d407f54038311598bc11bb921f6f489950
Natalija Germek <n.germek@icloud.com>
2023-01-27 01:15:37 -0800

    updated URLS, README, views, model, serializer and settings.py

---
+++ pets_app/serializer.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from .models import Pet
+
+
+# the serializer allows us to grab the data and convert into json data in the modal, it will also parse the data and
+class PetSerializer(serializers.ModelSerializer):
+    # need meta class to provide access to database
+    class Meta:
+        fields = ('pet_name', 'pet_owner', 'pet_description', 'created_at')
+        model = Pet
--- pets_app/views.py
+++ pets_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Pet
+from .serializer import PetSerializer
 
-# Create your views here.
+
+class PetsList(generics.ListCreateAPIView):
+    queryset = Pet.objects.all()
+    serializer_class = PetSerializer
+
+
+class PetDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Pet.objects.all()
+    serializer_class = PetSerializer
